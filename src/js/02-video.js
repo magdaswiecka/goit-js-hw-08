ff42581be96900c34f37380ff273d0714bd92af7
magdaswiecka <magstaron@gmail.com>
2022-10-31 21:43:15 +0100

    Drugie zadanie

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const rememberedSeconds = localStorage.getItem("videoplayer-current-time");
+
+if(rememberedSeconds) {
+  player.setCurrentTime(rememberedSeconds)
+}
+
+const func = (data) => {
+  console.log('timeupdate!', data.seconds);
+
+  localStorage.setItem("videoplayer-current-time", data.seconds)
+};
+
+const throttledFunc = throttle(func, 1000);
+
+player.on('timeupdate', throttledFunc);
 
