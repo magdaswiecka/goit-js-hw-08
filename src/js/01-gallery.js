d6757789cfea00b30d1b8481adfece15bb553d41
magdaswiecka <magstaron@gmail.com>
2022-10-31 21:42:57 +0100

    Pierwsze zadanie

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const myGallery = document.querySelector(".gallery");
+
+for(const galleryItem of galleryItems) {
+  myGallery.insertAdjacentHTML("beforeend", `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        alt="${galleryItem.description}"
+      />
+    </a>
+  </div>
+  `)
+}
+
+
+var lightbox = new SimpleLightbox('.gallery a.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250
+});
