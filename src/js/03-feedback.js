84a576a74cf9952a34866b6881ec7d901dea3add
magdaswiecka <magstaron@gmail.com>
2022-10-31 21:43:27 +0100

    Trzecie zadanie

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form.feedback-form');
+let inputEmail = document.querySelector('input[name=email]');
+let inputMess = document.querySelector('textarea[name=message]');
+const submitBtn = document.querySelector('button[type=submit]');
+
+let formData = {
+  email: '',
+  message: '',
+}
+
+form.addEventListener('input', throttle(inputChange, 500));
+submitBtn.addEventListener('click', clearStorage);
+
+checkStorage();
+
+function inputChange(event) {
+  event.preventDefault();
+  
+  formData.email = inputEmail.value;
+  formData.message = inputMess.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function checkStorage() {
+  const localStorageValue = localStorage.getItem('feedback-form-state');
+
+  if(localStorageValue) {
+    formData = JSON.parse(localStorageValue);
+    inputEmail.value = formData.email;
+    inputMess.value = formData.message;
+  }
+}
+
+function clearStorage(event) {
+  event.preventDefault();
+  localStorage.clear();
+  form.reset();
+  console.log(formData);
+}
